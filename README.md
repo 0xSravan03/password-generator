c3a4e80c83fe3201a873db87ad3cea674d2a0b4f
Sravan T P <sravantp03@gmail.com>
2023-10-15 16:00:15 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Password Generator
+
+## A React application which helps user to generate password.
+
+### Features
+
+1. Password size can be adjusted by using a slider.
+2. Options to include Numbers and Special characters.
+3. Button to copy the password for better use.
+
+### Techstack Used
+
+- React
+- Tailwind Css
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 
 export default function App() {
-  const [length, setLength] = useState(8);
+  const [length, setLength] = useState(10);
   const [isNumberAllowed, setIsNumberAllowed] = useState(false);
   const [isCharacterAllowed, setIsCharacterAllowed] = useState(false);
   const [password, setPassword] = useState('');
+  const [copied, setCopied] = useState(false);
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -23,8 +25,20 @@ export default function App() {
       _password += str[Math.floor(Math.random() * str.length)];
     }
 
-    setPassword(_password); // updating state variable password with newly created passsword
+    setPassword(_password); // updating state variable password with newly created password
   }, [length, isCharacterAllowed, isNumberAllowed]);
+
+  function copyPasswordToClip() { 
+    setCopied(true);
+
+    // copy password to clipboard
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+    
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  }
 
   useEffect(() => {
     passwordGenerator();
@@ -34,11 +48,11 @@ export default function App() {
     <div className='w-full max-w-md bg-gray-800 text-orange-500 mx-auto shadow-md rounded-lg px-4 py-5 my-20'>
       <h1 className='text-center text-white my-3 text-2xl'>Password Generator</h1>
       <div className='w-full flex flex-row gap-4 shadow rounded-lg justify-between px-4 my-4'>
-        <input type="text" value={password} className='w-full outline-none px-2 py-2 bg-white rounded-md font-semibold text-black'  placeholder='password' disabled />
-        <button className='px-3 rounded-xl bg-blue-400 outline-none text-white font-semibold hover:scale-110 transition-all' >copy</button>
+        <input type="text" value={password} className='w-full outline-none px-2 py-2 bg-white rounded-md font-semibold text-black'  placeholder='password' disabled ref={passwordRef} />
+        <button className='px-4 rounded-xl bg-blue-400 outline-none text-white font-semibold hover:scale-110 transition-all' onClick={copyPasswordToClip} >{!copied ? `copy` : `copied`}</button>
       </div>
       <div className='w-full px-4 flex center justify-between flex-wrap py-1.5'>
-        <input id="range" name="passwordSize" className='hover:cursor-pointer' type="range" min={6} max={20} defaultValue={8} onChange={(e) => setLength(e.target.value)} />{`${length}`}
+        <input id="range" name="passwordSize" className='hover:cursor-pointer' type="range" min={8} max={20} defaultValue={10} onChange={(e) => setLength(e.target.value)} />{`${length}`}
         <div className='flex items-center justify-center gap-2'>
           <label htmlFor='isNumber'>Numbers</label>{" "}<input type="checkbox" id="isNumber" onChange={(e) => setIsNumberAllowed(e.target.checked)} className='hover:cursor-pointer' />
         </div>
